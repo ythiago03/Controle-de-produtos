3ca12a2b758419e4edc86e93c55c3f2dbde48dd4
Thiago Henrique <thiagohcfidencio@gmail.com>
2023-02-15 16:48:21 -0300

    Adding elements on array

--- js/index.js
+++ js/index.js
@@ -23,23 +23,59 @@ class Product {
         this.products = [];
     }
 
-
+    
     save(){
-       let product = this.getData();
-       console.log(product);
-       this.id++;
+        let product = this.#getData();
+        if(product === 'invalid'){
+            return
+        }
+
+        this.products.push(product)
+        this.clear()
+        this.#renderProducts()
+        this.id++;
     }
 
-   getData(){
+   #getData(){
     let product = {}
 
     product.id = this.id;
     product.name = document.querySelector('#productName').value;
     product.value = document.querySelector('#productValue').value;
-
+      
+    if(this.#verifyData(product)){
+        this.clear()
+        return 'invalid'
+    }
+    
     return product
    }
 
+   #verifyData({ name, value }){
+        let errorMessage = ''
+
+        if(name === ''){
+            errorMessage += 'Oops! Insira um nome válido! \n'
+        }
+        if(value === ''){
+            errorMessage += 'Oops! Insira um valor válido! \n'
+        }
+
+        if(errorMessage != ''){
+            alert(errorMessage)
+            return true
+        }
+        return false
+   }
+
+   clear(){
+        document.querySelector('#productName').value = ''
+        document.querySelector('#productValue').value = ''
+   }
+
+   #renderProducts(){
+    console.log(this.products);
+   }
 }
 
 let product = new Product()
